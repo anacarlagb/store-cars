cc5fff1aa5f353d67b54908fe2df6159e4803e86
anacarlagb <ana.carlagb@gmail.com>
2023-08-11 18:04:43 -0300

    feat: creating store of cars

---
+++ src/repository/bd.py
@@ -0,0 +1,17 @@
+Carros = [
+
+
+    {
+        'id': 1,
+        'marca': 'Fiat',
+        'modelo': 'Marea',
+        'ano': 1990
+    },
+
+    {
+        'id': 2,
+        'marca': 'Fiat',
+        'modelo': 'Marea',
+        'ano': 1995
+    },
+]
